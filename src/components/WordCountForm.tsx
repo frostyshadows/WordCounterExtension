938432e729ff713ca693ab9dd34c09541d2ab517
Russell Blickhan <rwblickhan@gmail.com>
2022-12-22 18:29:44 -0600

    Convert components to function components

--- src/components/WordCountForm.tsx
+++ src/components/WordCountForm.tsx
@@ -1,42 +1,28 @@
-import React, { ChangeEvent, FormEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { Button } from "@material-tailwind/react";
 
-interface Props {}
+export default function WordCountForm() {
+  let [count, setCount] = useState("");
 
-interface State {
-  count: string;
-}
-
-export default class WordCountForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { count: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const input = (event.target as HTMLInputElement).value;
     const numbersOnlyInput = input.replace(/\D/g, "");
-    this.setState({ count: numbersOnlyInput });
+    setCount(numbersOnlyInput)
   }
 
-  handleSubmit() {
-    alert("A word count was submitted: " + this.state.count);
+  function handleSubmit() {
+    alert("A word count was submitted: " + count);
   }
 
-  render() {
-    return (
-      <div>
-        <input
-          className="border-2"
-          type="text"
-          value={this.state.count}
-          onChange={this.handleChange}
-        />
-        <Button onClick={() => this.handleSubmit()}>Log</Button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <input
+        className="border-2"
+        type="text"
+        value={count}
+        onChange={handleChange}
+      />
+      <Button onClick={handleSubmit}>Log</Button>
+    </div>
+  );
 }
--- src/components/WordCountProgressBar.tsx
+++ src/components/WordCountProgressBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Progress } from "@material-tailwind/react";
 
 interface Props {
@@ -7,22 +6,13 @@ interface Props {
   period: string;
 }
 
-interface State {}
-
-export default class WordCountProgressBar extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  render() {
-    const progress = (this.props.current / this.props.target) * 100;
-    return (
-      <div>
-        <Progress value={progress} variant="filled" />
-        <p>
-          {this.props.current} / {this.props.target} words {this.props.period}
-        </p>
-      </div>
-    );
-  }
+export default function WordCountProgressBar({ current, target, period }: Props) {
+  return (
+    <div>
+      <Progress value={(current / target) * 100} variant="filled" />
+      <p>
+        {current} / {target} words {period}
+      </p>
+    </div>
+  );
 }

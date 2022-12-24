7f78b03361d4a00e3ed781db11e6cb12adb7cd33
Russell Blickhan <rwblickhan@gmail.com>
2022-12-24 09:17:09 -0800

    Create basic authentication (#3)
    * Start building out dropdown
    
    * Get basic auth working
    
    * PR updates

---
+++ src/components/ProjectDropdown.tsx
@@ -0,0 +1,30 @@
+import Dropdown from "./Dropdown";
+import { supabase } from "../supabase";
+import { useEffect, useState } from "react";
+import { Project } from "../exportedDbTypes";
+
+export default function ProjectDropdown() {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    getProjects();
+  });
+
+  const getProjects = async () => {
+    let { data, error } = await supabase.from("Project").select();
+
+    if (data) {
+      setProjects(projects);
+    } else if (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <Dropdown<Project>
+      options={projects}
+      optionName={(project) => project.title}
+      handleSelection={() => {}}
+    />
+  );
+}

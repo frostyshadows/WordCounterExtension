8d4122c64974562ec6b10ed985cb77a6aa39f9e7
Russell Blickhan <rwblickhan@gmail.com>
2022-12-25 13:42:13 -0600

    Remove supabase

--- src/service/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseKey = process.env.SUPABASE_SECRET_KEY as string;
-
-const supabaseClient = createClient(supabaseUrl, supabaseKey);

a9f7452270baf17a95d56946636cd73a3c2051de
Russell Blickhan <rwblickhan@gmail.com>
2022-12-25 13:15:54 -0600

    Revert "Create basic authentication (#3)"
    This reverts commit 7f78b03361d4a00e3ed781db11e6cb12adb7cd33.

--- src/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "./database.types";
-
-export const supabase = createClient<Database>(
-  "https://pjbvpypxedzggytqsrny.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqYnZweXB4ZWR6Z2d5dHFzcm55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE3Mjk1OTIsImV4cCI6MTk4NzMwNTU5Mn0.x0sAnEnmtkM3gbes23Z5Gec0uGPGAwHtQagR1pXTbvw"
-);
